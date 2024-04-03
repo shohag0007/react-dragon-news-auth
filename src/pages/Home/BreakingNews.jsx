c7373bd5276896913c261b18d34d1fc49f3bc5c5
shohag0007 <merinedrive007@gmail.com>
2024-04-04 01:32:03 +0600

    working on dragon news

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const BreakingNews = () => {
     return (
-        <div className="flex">
+        <div className="flex bg-rose-500 font-semibold text-white text-2xl">
             <button className="btn btn-secondary">Breaking News</button>
             <Marquee pauseOnHover={true} speed={100}>
-                <Link className="mr-12" to="/">I can be a React component........</Link>
-                <Link className="mr-12" to="/">I can be a React component........</Link>
-                <Link className="mr-12" to="/">I can be a React component........</Link>
-  I can be a React component, multiple React components, or just some text.
+                <Link className="mr-12" to="/">...........Shohag........</Link>
+                <Link className="mr-12" to="/">..........Breaking News........</Link>
+                <Link className="mr-12" to="/">...........Shohag News Gallery........</Link>
+  Thanks Shohag..........!!!
 </Marquee>
         </div>
     );
